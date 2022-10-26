f9aff614112d9c1e71dd18e9fb6b2602e3e8c95b
abdul momin <mominurrahman10748@gmail.com>
2022-10-26 16:30:21 +0600

    course&course details json file updated

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require('cors');
+app.use(cors())
+
+const course = require('./data/courses.json');
+app.get('/courses', (req, res) => {
+    res.send(course)
+})
+
 app.get('/', (req, res) => {
     res.send('Welcome to learning is easy server');
 })
 
+
 app.listen(port, () => {
-    console.log(`server is running ${port}`)
+    console.log(`learning is easy server running ${port}`)
 })
 
 
