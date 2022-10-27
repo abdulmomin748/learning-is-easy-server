25bd21ad14c87e6ebc3db51810a655b9a6df1b6a
abdul momin <mominurrahman10748@gmail.com>
2022-10-27 16:04:14 +0600

    checked

--- index.js
+++ index.js
@@ -5,9 +5,20 @@ const cors = require('cors');
 app.use(cors())
 
 const course = require('./data/courses.json');
+const courseDeatails = require('./data/course_details.json');
 app.get('/courses', (req, res) => {
     res.send(course)
 })
+
+app.get('/courses/:id', (req, res) => {
+    
+    const id = req.params.id;
+
+    const courseUnique = courseDeatails.find(courseUnique => courseUnique.id === id);
+    res.send(courseUnique);
+    console.log(req.params.id);
+    
+})    
 
 app.get('/', (req, res) => {
     res.send('Welcome to learning is easy server');
